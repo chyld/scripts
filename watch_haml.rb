e86d0bd5ea9105e24ba4163b569ebe69d9a607cd
Chyld Medford <chyld.medford@gmail.com>
2013-09-20 15:47:18 -0500

    updated haml watcher

--- watch_haml.rb
+++ watch_haml.rb
@@ -1,5 +1,12 @@
+puts `clear`
+puts 'Watching haml files...'
+
 watch('(.*?\.)haml$') do |path|
   cmd = "haml #{path[0]} #{path[1]}html"
-  `#{cmd}`
   puts cmd
+
+  begin
+    `#{cmd}`
+  rescue
+  end
 end
