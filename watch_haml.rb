57d3af8825dd865903795f8d64b642204658f843
Chyld Medford <chyld.medford@gmail.com>
2013-09-21 20:59:12 -0500

    added partials to haml parsing

--- watch_haml.rb
+++ watch_haml.rb
@@ -7,7 +7,10 @@ puts "Watching the #{directory} directory for HAML additions or modifications...
 listener = Listen.to(directory) do |modified, added, removed|
   (modified + added).each do |file|
 
-    if /haml$/.match(file)
+    # strip the filename from the full pathname
+    # do not convert partial haml files
+    local = File.basename(file)
+    if /^[^_].*haml/.match(local)
       cmd = "haml #{file} #{file.gsub(/haml$/, 'html')}"
       puts "#{Time.now.strftime("%H:%M:%S")}: #{cmd}"
 
