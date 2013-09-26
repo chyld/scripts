69112a054b233de7e6824dc109fd6adeafd193dc
Chyld Medford <chyld.medford@gmail.com>
2013-09-25 20:31:26 -0500

    updates parent when partial is saved

--- watch_haml.rb
+++ watch_haml.rb
@@ -2,7 +2,7 @@ require 'listen'
 
 directory = Dir.pwd
 puts `clear`
-puts "Watching the #{directory} directory for HAML additions or modifications..."
+puts "Watching the #{directory} directory for changes"
 
 listener = Listen.to(directory) do |modified, added, removed|
   (modified + added).each do |file|
@@ -10,16 +10,30 @@ listener = Listen.to(directory) do |modified, added, removed|
     # strip the filename from the full pathname
     # do not convert partial haml files
     local = File.basename(file)
+
+    # convert regular haml file to html
     if /^[^_].*haml/.match(local)
       cmd = "haml #{file} #{file.gsub(/haml$/, 'html')}"
       puts "#{Time.now.strftime("%H:%M:%S")}: #{cmd}"
-
       begin
         `#{cmd}`
       rescue
       end
     end
 
+    # if a haml partial is modified, cause it's parent to re-compile
+    if /^_.*haml/.match(local)
+      cmd = "grep -ril #{local} #{directory}"
+      files = `#{cmd}`.split
+      files.each do |f|
+        cmd = "touch #{f}"
+        puts "#{Time.now.strftime("%H:%M:%S")}: #{cmd}"
+        begin
+          `#{cmd}`
+        rescue
+        end
+      end
+    end
   end
 end
 
